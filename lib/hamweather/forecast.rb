5738420506c79fde38f37c3e8bac079748f1664b
David Lowry <davidlowry@davesmac.lan>
2008-12-02 15:44:00 +0000

    Updated forecast

--- lib/hamweather/forecast.rb
+++ lib/hamweather/forecast.rb
@@ -2,6 +2,9 @@ module Hamweather
   
   #<FPeriod interval="1" Day="FRI" Date="2008-11-14" Wx="Chance T-storms" Icon="tstorm.gif" HiF="66" HiC="19" LoF="56" LoC="13" Pop="60" Detail="Occasional showers with a chance of thunderstorms. Areas of fog. Some thunderstorms May be severe after midnight. Lows in the mid 50s. South winds 5 to 10 mph shifting to the southwest 10 to 15 mph after midnight. Chance of rain near 100 percent."/>    
   class Forecast    
+    
+    attr_accessor :high_farenheit, :high_celsius, :low_farenheit, :low_celsius, :day, :date, :expected_weather, :detail, :percentage_precipitation
+
     #high_farenheit = FPeriod/HiF
     #high_celsius = FPeriod/HiC
     #low_farenheit = FPeriod/LoF
@@ -11,8 +14,12 @@ module Hamweather
     #expected_weather = FPeriod/Wx
     #detail = FPeriod/Detail
     #percentage_preciptiation = FPeriod/Pop
+
     def new(xml_data)
-      
+      doc = Hpricot.parse(xml_data)
+      (doc/:FPeriod).each do |forecast|
+        
+      end
       @high_farenheit = ""
     end
     
