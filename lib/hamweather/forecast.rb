f3a446fdef07dc56760a4f8496d2892d0f5c9838
David Lowry <davidlowry@davesmac.lan>
2008-12-02 15:13:33 +0000

    Updated starter forecast spec

--- lib/hamweather/forecast.rb
+++ lib/hamweather/forecast.rb
@@ -1,7 +1,35 @@
 module Hamweather
   
-  class Forecast
+  #<FPeriod interval="1" Day="FRI" Date="2008-11-14" Wx="Chance T-storms" Icon="tstorm.gif" HiF="66" HiC="19" LoF="56" LoC="13" Pop="60" Detail="Occasional showers with a chance of thunderstorms. Areas of fog. Some thunderstorms May be severe after midnight. Lows in the mid 50s. South winds 5 to 10 mph shifting to the southwest 10 to 15 mph after midnight. Chance of rain near 100 percent."/>    
+  class Forecast    
+    #high_farenheit = FPeriod/HiF
+    #high_celsius = FPeriod/HiC
+    #low_farenheit = FPeriod/LoF
+    #low_celsius = FPeriod/LoC
+    #day = FPeriod/Day
+    #date = FPeriod/Date
+    #expected_weather = FPeriod/Wx
+    #detail = FPeriod/Detail
+    #percentage_preciptiation = FPeriod/Pop
+    def new(xml_data)
+      
+      @high_farenheit = ""
+    end
     
+    def parse_xml (xml_uri)
+      FIELDS = %w[HiF HiC LoF LoC Day Date Wx Detail Pop]
+
+      doc = Hpricot.parse(File.read(xml_uri))
+      (doc/:product).each do |xml_product|
+        product = Product.new
+        for field in FIELDS
+          product[field] = (xml_product/field.intern).first.innerHTML
+        end
+        product.save
+      end
+      
+    end
+
   end
   
 end
--- specs/forecast_spec.rb
+++ specs/forecast_spec.rb
@@ -12,8 +12,42 @@ describe Hamweather::Forecast do
     @forecast = Hamweather::Forecast.new(test_forecast_data)
   end
 
-  it "high_farenheit should be 56 degrees" do
+  it "high_farenheit should be 66 degrees" do
+    
+  end
+  
+  it "high_celsius should be 19 degrees" do
     
   end
 
+  it "low_farenheit should be 56 degrees" do
+
+  end
+  
+  it "low_celsius should be 13 degrees" do
+
+  end
+  
+  it "day should be friday" do
+    
+  end
+  
+  it "date should be the 14th of November 2008" do
+
+  end
+  
+  it "expected_weather should be Chance T-storms" do
+
+  end
+  
+  it "detail should be Occasional showers with a chance of thunderstorms. Areas of fog. Some thunderstorms May be severe after midnight. Lows in the mid 50s. South winds 5 to 10 mph shifting to the southwest 10 to 15 mph after midnight. Chance of rain near 100 percent." do
+
+  end
+  
+  it "percentage_preciptiation should be 60 percent" do
+  
+  end
+  
 end
+
+
