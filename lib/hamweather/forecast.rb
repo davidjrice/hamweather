bc4d6d9a1cc7bf8fb49940c7ac531dc43a11c1e8
David Lowry <davidlowry@davesmac.lan>
2008-12-02 16:19:24 +0000

    Hpricot fun

--- lib/hamweather/forecast.rb
+++ lib/hamweather/forecast.rb
@@ -1,6 +1,6 @@
 module Hamweather
   
-  #<FPeriod interval="1" Day="FRI" Date="2008-11-14" Wx="Chance T-storms" Icon="tstorm.gif" HiF="66" HiC="19" LoF="56" LoC="13" Pop="60" Detail="Occasional showers with a chance of thunderstorms. Areas of fog. Some thunderstorms May be severe after midnight. Lows in the mid 50s. South winds 5 to 10 mph shifting to the southwest 10 to 15 mph after midnight. Chance of rain near 100 percent."/>    
+  #forecast = Hpricot.parse('<FPeriod interval="1" Day="FRI" Date="2008-11-14" Wx="Chance T-storms" Icon="tstorm.gif" HiF="66" HiC="19" LoF="56" LoC="13" Pop="60" Detail="Occasional showers with a chance of thunderstorms. Areas of fog. Some thunderstorms May be severe after midnight. Lows in the mid 50s. South winds 5 to 10 mph shifting to the southwest 10 to 15 mph after midnight. Chance of rain near 100 percent."/>').root
   class Forecast    
     
     attr_accessor :high_farenheit, :high_celsius, :low_farenheit, :low_celsius, :day, :date, :expected_weather, :detail, :percentage_precipitation
@@ -16,27 +16,18 @@ module Hamweather
     #percentage_preciptiation = FPeriod/Pop
 
     def new(xml_data)
-      doc = Hpricot.parse(xml_data)
-      (doc/:FPeriod).each do |forecast|
-        
-      end
-      @high_farenheit = ""
-    end
-    
-    def parse_xml (xml_uri)
-      FIELDS = %w[HiF HiC LoF LoC Day Date Wx Detail Pop]
-
-      doc = Hpricot.parse(File.read(xml_uri))
-      (doc/:product).each do |xml_product|
-        product = Product.new
-        for field in FIELDS
-          product[field] = (xml_product/field.intern).first.innerHTML
-        end
-        product.save
-      end
+      forecast = Hpricot.parse(xml_data).root
       
+      @high_farenheit = forecast[:hif]
+      @high_celsius = forecast[:hic]
+      @low_farenheit = forecast[:lof]
+      @low_celsius = forecast[:loc]
+      @day = forecast[:day]
+      @date = forecast[:date]
+      @expected_weather = forecast[:wx]
+      @detail = forecast[:detail]
+      @percentage_preciptiation = forecast[:pop]
     end
 
-  end
-  
+  end  
 end
