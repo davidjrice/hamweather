eb7bdc2a0ed6f6818a60e740ab0dc97cb2142595
David Rice <me@davidjrice.co.uk>
2008-12-04 17:11:29 +0000

    Add notes about locations

--- lib/hamweather/location.rb
+++ lib/hamweather/location.rb
@@ -2,6 +2,17 @@ module Hamweather
   
   class Location
     
+    def initialize(data)
+      # is it a zipcode? then we can request the api directly
+      # is it a place name? geocode it. then request the api
+      
+      # http://hwlite.hamweather.net/wxACCESSID/wx/ZIPCODE.xml (US)
+      # http://hwlite.hamweather.net/wxACCESSID/wx/POSTALCODE.xml (CANADA)
+      # http://hwlite.hamweather.net/wxA1B2C3D4E5/wx/nearby.xml?lat=33.76&lon=-84.42 
+      
+      
+    end
+    
   end
   
 end
