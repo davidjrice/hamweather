ae6b8f188ca8e93fa52c03dcef9c5ce0a0fd9783
David Rice <me@davidjrice.co.uk>
2008-12-08 13:19:55 +0000

    Some more direction for location class

--- spec/location_spec.rb
+++ spec/location_spec.rb
@@ -2,19 +2,44 @@ require File.dirname(__FILE__) + '/spec_helper'
 
 describe Hamweather::Location do
   
-  #locations hash
-  #locations[:test][:data] = "str"
-  #locations[:test][:result] = true/false
-  locations[:zipcode][:data] = "12834" #Returns Greenwich, NY, USA
-  locations[:ca_postcode][:data] = "H0H0H0" #Returns Santa, Lapland, CA
-  locations[:geocode_single][:data] = "575 Burton Road, Greenwich, NY, 12834"  #Returns one option
-  locations[:geocode_multi][:data] = "Greenwich, USA"  #Returns 10 options, nb, no 'NY' in search string
-  
-  before(:each) do
-    @location = Hamweather::Location.new()
+  describe "with zipcode 12834" do
+    
+    # should be a valid zipcode (Greenwich, NY, USA)
+    
+    before(:each) do
+      @location = Hamweather::Location.new("12834")
+    end
+    
+    it ".to_uri" do
+      @location.to_uri.should == "/wx/12834.xml"
+    end
+    
   end
-
+  
+  describe "with postcode H0H0H0" do
+    
+    # should be a valid postcode (Santa, Lapland, CA)
+    
+    before(:each) do
+      @location = Hamweather::Location.new("H0H0H0")
+    end
+    
+    it ".to_uri" do
+      @location.to_uri.should == "/wx/H0H0H0.xml"
+    end
+    
+  end
+  
+  describe "with an unambiguous address, '575 Burton Road, Greenwich, NY, 12834" do
+    #Returns one option
+  end
+  
+  describe "with an ambiguous address, 'Greenwich, USA" do
+    #Returns 10 options, nb, no 'NY' in search string
+  end
+  
   it "Location.parse should return xml string when zipcode" do
     pending
   end
+  
 end
